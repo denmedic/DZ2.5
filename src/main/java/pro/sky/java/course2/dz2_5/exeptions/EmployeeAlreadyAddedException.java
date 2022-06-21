c450858897f2d9100f5f5946b7d3229d6c95ac48
denisskarednyh <deniska2019@yandex.ru>
2022-06-21 12:10:40 +0300

    1

--- src/main/java/pro/sky/java/course2/dz2_5/exeptions/EmployeeAlreadyAddedException.java
+++ src/main/java/pro/sky/java/course2/dz2_5/exeptions/EmployeeAlreadyAddedException.java
@@ -1,4 +1,8 @@
 package pro.sky.java.course2.dz2_5.exeptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyAddedException extends RuntimeException{
 }
